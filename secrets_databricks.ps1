a178ae4d18ccae9322c32577f5ba4bec07dfc288
DavideCiarla <49128099+DavideCiarla@users.noreply.github.com>
2020-03-18 21:29:57 +0100

    Update secrets_databricks.ps1

--- secrets_databricks.ps1
+++ secrets_databricks.ps1
@@ -7,18 +7,18 @@ databricks configure --token
 https://westeurope.azuredatabricks.net/
 <token generato da databricks>
 
-databricks secrets create-scope --scope adlg2
+# databricks secrets create-scope --scope < scope name >
 
-databricks secrets create-scope --scope sc_adlg2 --initial-manage-principal users
+databricks secrets create-scope --scope < scope name > --initial-manage-principal users
 
-databricks secrets put --scope sc_adlg2 --key adlg2_key   # chiave dello storage account da prendere dal portale azure ì
+databricks secrets put --scope < scope name > --key < key name >   # chiave dello storage account da prendere dal portale azure ì
 
 #databricks secrets list-scopes
 
-#databricks secrets list --scope adlg2
+#databricks secrets list --scope < scope name >
 
 #####################################
 
 #da databricks 
 
-storage_account_key = dbutils.secrets.get("sc_adlg2", "adlg2_key")
+storage_account_key = dbutils.secrets.get("< scope name >", "< key name >")
